15251fc24ce99f0b5aac284c6e19621de3f92cbe
Steve Cosgrove <steve@rata.co.nz>
2022-07-06 16:09:09 +1200

    Added example of what to put in .bashrc

--- prompt.sh
+++ prompt.sh
@@ -4,6 +4,12 @@
 # Need to add machine details
 # PS1='${debian_chroot:+($debian_chroot)}\u@\h:(Fibre)\w\$ '
 
+# Add the following lines to ~/,bashrc
+# # work out what Internet connection are are connected to
+# NWTYPE=`~/scripts/prompt.sh`
+# PS1=${PS1//\\\$/\\($NWTYPE)$}
+
+
 a210_246_49_101=fibre
 a203_118_151_16=cable
 
@@ -15,7 +21,8 @@ NWIP=a${MYIP//./_}
 
 NWNAME=${!NWIP}
 
-export PS1='${debian_chroot:+($debian_chroot)}\u@\h:($NWNAME)\w\$ '
-
+# export PS1='${debian_chroot:+($debian_chroot)}\u@\h:($NWNAME)\w\$ '
+# Instead of changing prompt directly, output NWNAME as text
+echo $NWNAME
 
 
